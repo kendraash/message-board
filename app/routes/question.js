b6063f1094b26635c458c611f3b067d2ebde3206
Kendra Ash <kendra.ash32@gmail.com>
2015-09-26 09:13:32 -0700

    add delte function for each question

--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
+  },
+
+  actions: {
+    delete(question){
+      if(confirm("Are you sure you want to delete this question?")){
+        question.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
   }
 });
