0a96d391ca9568d977843404c9387fd9c51ca1e8
Kendra Ash <kendra.ash32@gmail.com>
2015-09-26 08:56:08 -0700

    add a component for a new question

---
+++ app/components/new-question.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+      questionFormShow() {
+        this.set('addNewQuestion', true)
+      },
+
+      save() {
+        var params = {
+          text: this.get('text'),
+          author: this.get('author'),
+          notes: this.get('notes')
+        };
+        this.set('addNewQuestion', false),
+        this.sendAction('save', params);
+
+      }
+  }
+});
