1c1c41d0efedbb087c8fb33cf44ceba5f3d81780
Kendra Ash <kendra.ash32@gmail.com>
2015-10-02 08:47:35 -0700

    add code to fix update for question and add attribute date to answer model

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   text: DS.attr(),
   author: DS.attr(),
+  date: DS.attr(),
   question: DS.belongsTo('question', { async: true})
 
 });
